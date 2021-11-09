62d1bfc73c8ce1e80a4e9c9cb577c0fb1ef65356
Cesar dos Santos de Almeida <cesarsantosss499@gmail.com>
2021-11-08 21:12:19 -0300

    Correção: resolução do bug  de caracteres especiais

--- App/pdf/geral/modelo-pdf.php
+++ App/pdf/geral/modelo-pdf.php
@@ -1,5 +1,4 @@
 <?php
-
 // Incluindo o autoload
 require_once '../../../vendor/autoload.php';
 
@@ -33,6 +32,7 @@ $quantidade_movimentacao = $resumo->contagemMovimentacao();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <title>Relatorio Geral</title>
 
     <style>
